321dc26095e21b5fd703a64108157fe47c894528
Sean Higgins <seansuke@gmail.com>
2013-03-23 13:10:02 -0400

    Touch Test

--- scripts/player.js
+++ scripts/player.js
@@ -65,24 +65,24 @@ function Player() {
 				this.action = "idle";
 			}
 			else {
-				if( key[87] || touch.pageY < camera.height * 1/4 && touch.pageX != -1 ) {
+				if( key[87] || touch.pageY < camera.height * 1/3 && touch.pageX != -1 ) {
 					this.y -= this.spd;
 					this.action = "run";
 					this.gob.gfx('run' + this.max(3) );
 				}
-				else if( key[83] || touch.pageY > camera.height * 3/4 && touch.pageX != -1 ) {
+				else if( key[83] || touch.pageY > camera.height * 2/3 && touch.pageX != -1 ) {
 					this.y += this.spd;
 					this.action = "run";
 					this.gob.gfx('run' + this.max(3) );
 				}
-				if( key[68] || touch.pageX > camera.width * 3/4 && touch.pageX != -1 ) {
+				if( key[68] || touch.pageX > camera.width * 2/3 && touch.pageX != -1 ) {
 					this.x += this.spd;
 					this.action = "run";
 					this.dir = "right";
 					this.gob.dir( this.dir );
 					this.gob.gfx('run' + this.max(3) );
 				}
-				else if( key[65]  || touch.pageX < camera.width * 1/4 && touch.pageX != -1 ) {
+				else if( key[65]  || touch.pageX < camera.width * 1/3 && touch.pageX != -1 ) {
 					this.x -= this.spd;
 					this.action = "run";
 					this.dir = "left";
